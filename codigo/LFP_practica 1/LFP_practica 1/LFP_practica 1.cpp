b6cebba1e58042aef2939999c0b0c7dffa2ecb18
Jonás-fg <3812317550101@ingenieria.usac.edu.gt>
2026-02-28 20:57:18 -0600

    [ADD] tercer commit: Creacion de los metodos para reportes de: (reporteEstqaadisticoPorCurso) y (reporteRendimientoPorEstudiante), y pequeñas modificaciones para que se vea mejor.

--- codigo/LFP_practica 1/LFP_practica 1/LFP_practica 1.cpp
+++ codigo/LFP_practica 1/LFP_practica 1/LFP_practica 1.cpp
@@ -8,10 +8,6 @@
 
 using namespace std;
 
-vector<Estudiante> estudiantes;
-vector<Curso> cursos;
-vector<Nota> notas;
-
 /* ======================================
          STRUCTS (MODELOS DE DATOS)
    ======================================*/
@@ -40,6 +36,10 @@ struct Nota {
     int anio;
 };
 
+vector<Estudiante> estudiantes;
+vector<Curso> cursos;
+vector<Nota> notas;
+
 
 /* ======================================
              FUNCION SPLIT
@@ -58,7 +58,7 @@ vector<string> split(string linea) {
 }
 
 /* ======================================
-         CARGAR ESTUDIANTES
+             CARGAR ESTUDIANTES
    ======================================*/
 
 void cargarEstudiantes(){
@@ -96,7 +96,7 @@ void cargarCursos(){
     ifstream archivo("cursos.lfp");
 
     if (!archivo.is_open()){
-        cout << "Error al abrir curos.lfp\n";
+        cout <<"Error al abrir curos.lfp\n";
         return;
     }
 
@@ -140,7 +140,7 @@ void cargarNotas(){
         if (datos.size()==5){
 
             Nota n;
-            n.carnet= stoi(datos[0]);
+            n.carnet=stoi(datos[0]);
             n.codigoCurso= stoi(datos[1]);
             n.nota= stod(datos[2]);
             n.ciclo= datos[3];
@@ -154,7 +154,193 @@ void cargarNotas(){
 }
 
 
-int main()
-{
-	cout << "Hola mundo";
+/* ======================================
+		OPERACIONES ESTADISTICAS
+   ====================================== */
+
+double calcularPromedio(vector<double> valores){
+
+    if(valores.empty())
+        return 0;
+
+    double suma=0;
+
+    for (double v : valores) {
+        suma += v;
+    }
+    return suma/valores.size();
+}
+
+
+double calcularMediana(vector<double> valores){
+
+    if (valores.empty())
+        return 0;
+
+    sort(valores.begin(), valores.end());
+
+    int n=valores.size();
+
+    if (n % 2==0){
+        return(valores[n/2-1]+valores[n/2])/2.0;
+    }
+    else{
+        return valores[n/2];
+    }
+}
+
+
+
+double calcularDesviacion(vector<double> valores) {
+
+    if (valores.empty())
+        return 0;
+
+    double promedio =calcularPromedio(valores);
+    double suma=0;
+
+    for (double v :valores) {
+        suma += pow(v - promedio, 2);
+    }
+
+    return sqrt(suma / valores.size());
+}
+
+
+
+double calcularMaximo(vector<double> valores) {
+
+    if (valores.empty())
+        return 0;
+
+    double maximo=valores[0];
+    for (int i=0; i<valores.size(); i++) {
+        if (valores[i]>maximo) {
+            maximo=valores[i];
+        }
+    }
+    return maximo;
+}
+
+
+double calcularMinimo(vector<double> valores) {
+
+    if (valores.empty())
+        return 0;
+
+    double minimo=valores[0];
+    for (int i=0; i < valores.size(); i++) {
+        if (valores[i]<minimo) {
+            minimo = valores[i];
+        }
+    }
+    return minimo;
+}
+
+
+
+   /*======================================
+      REPORTE
+    ======================================*/
+
+struct Ranking {
+    string nombreCompleto;
+    double promedio;
+};
+
+ void reporteEstadisticasPorCurso(){
+
+     for (Curso cur:cursos){
+
+         vector<double> notasCurso;
+         int aprobados=0;
+         int reprobados =0;
+
+         for (Nota n:notas){
+
+             if (n.codigoCurso==cur.codigo){
+                 notasCurso.push_back(n.nota);
+
+                 if (n.nota>=61)
+                     aprobados++;
+                 else
+                     reprobados++;
+             }
+         }
+
+         if (!notasCurso.empty()){
+
+             double promedio= calcularPromedio(notasCurso);
+             double mediana=calcularMediana(notasCurso);
+             double desviacion=calcularDesviacion(notasCurso);
+             double maximo =calcularMaximo(notasCurso);
+             double minimo = calcularMinimo(notasCurso);
+             double porcentajeAprobacion=(double)aprobados/notasCurso.size() * 100;
+                
+             cout << "\n====================================\n";
+             cout << "Curso: "<< cur.nombre << endl;
+             cout << "Promedio: "<< promedio << endl;
+             cout << "Mediana: "<< mediana << endl;
+             cout << "Desviacion: "<< desviacion << endl;
+             cout << "Nota maxima: " << maximo<< endl;
+             cout << "Nota minima: " << minimo<< endl;
+             cout << "Aprobados: "<< aprobados << endl;
+             cout << "Reprobados: "<< reprobados << endl;
+             cout << "Porcentaje aprobacion: "<<porcentajeAprobacion << "%\n";
+         }
+     }
+ }
+
+
+
+ void reporteRendimientoPorEstudiante() {
+
+     for (Estudiante estu :estudiantes){
+
+         vector<double> notasEstudiante;
+         int aprobados=0;
+         int reprobados =0;
+         int creditosAcumulados= 0;
+
+         for (Nota n:notas){
+
+             if (n.carnet == estu.carnet){
+
+                 notasEstudiante.push_back(n.nota);
+
+                 if (n.nota >= 61){
+                     aprobados++;
+                     // Buscar el curso para sumar créditos
+                     for (Curso cur:cursos){
+                         if (cur.codigo == n.codigoCurso) {
+                             creditosAcumulados += cur.creditos;
+                             break;
+                         }
+                     }
+                 }
+                 else{
+                     reprobados++;
+                 }
+             }
+         }
+
+         if (!notasEstudiante.empty()) {
+             double promedio =calcularPromedio(notasEstudiante);
+
+             cout << "\n====================================\n";
+             cout << "Estudiante: " << estu.nombre << " " << estu.apellido << endl;
+             cout << "Promedio general: " << promedio << endl;
+             cout << "Cursos aprobados: " << aprobados << endl;
+             cout << "Cursos reprobados: " << reprobados << endl;
+             cout << "Creditos acumulados: " << creditosAcumulados << endl;
+         }
+     }
+ }
+
+
+
+int main(){
+
+
+    return 0;
 }
